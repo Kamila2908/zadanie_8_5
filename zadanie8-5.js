75937ea99585a301b68aeb0ba12c9844c3f5fabb
Kamila Dudkiewicz <dudkiewicz.kamila@yahoo.com>
2018-03-03 22:53:08 +0300

    Ready exercise 8.5

---
+++ zadanie8-5.js
@@ -0,0 +1,6 @@
+var a = prompt('Enter lenght of the base'),
+    h = prompt('Enter lenght of the height');
+
+triangleArea = a*h/2
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
